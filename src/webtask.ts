1ab8aa520ffc30d09e19b4b761f30239d8e280f9
Tommy Anderson <tommy2030@gmail.com>
2017-11-10 14:09:15 -0500

    Add publish command

--- src/webtask.ts
+++ src/webtask.ts
@@ -1,3 +1,15 @@
-export default (context: any, cb: any) => {
-    cb(null, { hello: context.query.name || 'Anonymous' });
-};
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+const Webtask = require('webtask-tools');
+
+// init Express context
+const app = express();
+app.use(bodyParser.json());
+
+// setup routes
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
+// export instance
+module.exports = Webtask.fromExpress(app);
