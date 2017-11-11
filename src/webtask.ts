ec38e92033643030db7360b68029a75f76f25383
Tommy Anderson <tommy2030@gmail.com>
2017-11-10 19:34:29 -0500

    Add initial packet routing logic

--- src/webtask.ts
+++ src/webtask.ts
@@ -2,14 +2,49 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 const Webtask = require('webtask-tools');
 
+import { Packet, validatePacket } from './packet';
+import { Storage } from './storage';
+
 // init Express context
 const app = express();
 app.use(bodyParser.json());
 
+// prepare global storage container
+const storage = new Storage();
+
 // setup routes
-app.get('/', (req, res) => {
+app.post('/packet', (req, res) => {
+    const packet = req.body as Packet;
+
+    // validate the request
+    const err = validatePacket(packet);
+    if (err) {
+        return res.status(400).send(err);
+    }
+
+    // store the packet
+    storage.queuePacket(packet);
+
     res.sendStatus(200);
 });
 
-// export instance
-module.exports = Webtask.fromExpress(app);
+app.get('/packet', (req, res) => {
+    const key = req.body;
+    if (typeof key !== 'string') {
+        return res.status(400).send('Must provide a valid key');
+    }
+
+    // retrieve and send all outstanding packets
+    const packets = storage.dequeuePackets(key);
+
+    res.status(200).send(packets);
+});
+
+// intercept the context storage for deserializing, as well as serializing + caching at the end
+module.exports = ((ctx: any, cb: any) => {
+    storage.deserialize(ctx, cb);
+
+    Webtask.fromExpress(app)(ctx, cb);
+
+    storage.serialize(ctx, cb);
+});
